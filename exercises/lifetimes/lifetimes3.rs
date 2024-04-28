4083b8493ed666db23be2b2c59aaabdad0cdf480
CybCom <91059829+CybCom@users.noreply.github.com>
2024-04-28 22:26:56 +0800

    feat(lifetimes): :sparkles: Lifetimes fin. ch10

--- exercises/lifetimes/lifetimes3.rs
+++ exercises/lifetimes/lifetimes3.rs
@@ -5,11 +5,11 @@
 // Execute `rustlings hint lifetimes3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+// I AM \NOT DONE
 
-struct Book {
-    author: &str,
-    title: &str,
+struct Book<'thisCanBe_ThatLong123> {
+    author: &'thisCanBe_ThatLong123 str,
+    title: &'thisCanBe_ThatLong123 str,
 }
 
 fn main() {
