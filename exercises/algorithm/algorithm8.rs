86d13d3ddd8936d1b0aae79683dbcb093cb215c9
CybCom <91059829+CybCom@users.noreply.github.com>
2024-05-01 23:25:33 +0800

    feat(algorithm): :sparkles: algo 7 & 8

--- exercises/algorithm/algorithm8.rs
+++ exercises/algorithm/algorithm8.rs
@@ -2,7 +2,9 @@
 	queue
 	This question requires you to use queues to implement the functionality of the stac
 */
-// I AM NOT DONE
+// I AM \NOT DONE
+
+use std::mem::swap;
 
 #[derive(Debug)]
 pub struct Queue<T> {
@@ -68,14 +70,24 @@ impl<T> myStack<T> {
     }
     pub fn push(&mut self, elem: T) {
         //TODO
+        self.q1.enqueue(elem);
     }
     pub fn pop(&mut self) -> Result<T, &str> {
         //TODO
-		Err("Stack is empty")
+        if self.q1.is_empty(){
+            return Err("Stack is empty")
+        }
+        while self.q1.size() > 1 {
+            let elem = self.q1.dequeue().unwrap();
+            self.q2.enqueue(elem);
+        }
+        let result = self.q1.dequeue().unwrap();
+        swap(&mut self.q1, &mut self.q2);
+        Ok(result)
     }
     pub fn is_empty(&self) -> bool {
 		//TODO
-        true
+        self.q1.is_empty()
     }
 }
 
